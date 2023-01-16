9f70f55af3541705256b52cb75dbdfd774cee03d
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-16 09:06:02 -0800

    task 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
+int find_len(char *s);
+char *copy(char *c1, char *c2);
 
 /**
  * new_dog - function that creates a new dog.
@@ -16,8 +18,68 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_ptr = malloc(sizeof(dog_ptr));
 	if (dog_ptr == NULL)
 		return (NULL);
-	dog_ptr->name = name;
 	dog_ptr->age = age;
-	dog_ptr->owner = owner;
+	dog_ptr->name = malloc((find_len(name) + 1) * sizeof(*name));
+	if (dog_ptr->name == NULL)
+	{
+		free(dog_ptr);
+		return (NULL);
+	}
+	else
+	{
+		copy(name, dog_ptr->name);
+	}
+	dog_ptr->owner = malloc((find_len(owner) + 1) * sizeof(*owner));
+	if (dog_ptr->owner == NULL)
+	{
+		free(dog_ptr->name);
+		free(dog_ptr);
+		return (NULL);
+	}
+	else
+	{
+		copy(owner, dog_ptr->owner);
+	}
 	return (dog_ptr);
+}
+
+/**
+ * find_len - find the str len
+ * @s: str
+ * Return: len
+ */
+
+int find_len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+/**
+ * copy - copy string
+ * @c1: str address
+ * @c2: paste c1 here
+ * Return: c2
+ */
+
+char *copy(char *c1, char *c2)
+{
+	int i, j;
+
+	i = 0;
+	j = find_len(c1);
+	while (i < j)
+	{
+		c2[i] = c1[i];
+		i++;
+	}
+	c2[i] = '\0';
+	return (c2);
 }
