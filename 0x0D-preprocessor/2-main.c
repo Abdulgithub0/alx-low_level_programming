3f5ec8a000452cc6f1faa8bdaa5bccd70a594fe8
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-17 06:22:39 -0800

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program to the the name of the src file it got compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
