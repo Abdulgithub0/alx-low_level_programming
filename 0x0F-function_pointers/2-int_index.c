85f6cfe652b4559d3f6f8afa69f4dacd467ab10f
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-18 04:35:32 -0800

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer.
+ * @array: to be acted upon by call back function invoked by cmp
+ * @size: size of the array
+ * @cmp: pointer to the call back func.
+ *
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if ((cmp((array[i])) != 0))
+				return (i);
+		}
+	}
+	return (-1);
+}
