5d08073c935f3dd27014b209d155c56b9246c7b2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-29 09:40:15 -0800

    task 4

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,5 +11,5 @@ void __attribute__((constructor)) execute_before_main(void);
 void execute_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-		"bore my house upon my back!\n");
+		"I bore my house upon my back!\n");
 }
