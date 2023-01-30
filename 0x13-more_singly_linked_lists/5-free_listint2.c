4f07f05367230f00ef5943a14159712bc23b3964
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-01-30 11:14:44 -0800

    fix free_list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,9 +15,9 @@ void free_listint2(listint_t **head)
 
 	while ((*head))
 	{
-		next_node = *head;
+		next_node = (*head)->next;
 		free(*head);
-		*head = next_node->next;
+		*head = next_node;
 	}
 	head = NULL;
 }
