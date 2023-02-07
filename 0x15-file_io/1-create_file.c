b009672ea3ac99eaab0d79c48af22d8ff773636f
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-02-07 03:45:22 -0800

    fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,8 @@ int create_file(const char *filename, char *text_content)
 	fdopen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fdopen < 0)
 		return (-1);
+	if (text_content == NULL)
+		text_content = '\0';
 	lenght = strlen(text_content);
 	fdwrite = write(fdopen, text_content, lenght);
 	if (fdwrite < 0)
