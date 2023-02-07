c77e8bb482d10ace58bf7c9da24896f70b509dce
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-02-06 16:13:20 -0800

    task2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,6 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	length = strlen(text_content);
+	if (!text_content)
+		length = 0;
 	open_text = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (open_text < 0)
 	{
