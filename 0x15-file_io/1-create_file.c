a93bdd9613b1ac54c9a1ef4f81e82b326f5fa9a1
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-02-06 16:06:48 -0800

    task2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file.
+ * @filename: text file to write to
+ * @text_content: string to read from
+ *
+ * Return: 1 or -1 if otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t open_text, total_write;
+
+	int length;
+
+	if (!filename)
+		return (-1);
+	length = strlen(text_content);
+	open_text = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (open_text < 0)
+	{
+		return (-1);
+	}
+	total_write = write(open_text, text_content, length);
+	if (total_write < 0 || total_write != length)
+	{
+		return (-1);
+	}
+	close(open_text);
+	return (0);
+}
