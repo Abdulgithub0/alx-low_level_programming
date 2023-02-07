fa738d6f5fb210641e29a88480872fc91dca672d
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-02-07 02:54:31 -0800

    fix error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,9 +21,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	length = strlen(text_content);
-	if (!text_content)
-		length = 0;
+	length = 0;
+	while (text_content[length] != '\0')
+		length++;
 	open_text = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (open_text < 0)
 	{
