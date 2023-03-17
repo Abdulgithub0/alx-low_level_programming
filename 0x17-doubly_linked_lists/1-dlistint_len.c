3e9556da476b38f9156d446c275982fd8614e8f6
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-17 15:17:32 +0100

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - func. to return total doubly linked nodes 
+ * @h: pointer to address of the head node
+ * Return: total number of the nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t total_node;
+
+	dlistint_t *traveler;
+
+	if (h == NULL)
+		return (0);
+	total_node = 1;
+	traveler = h->next;
+	while (traveler != NULL)
+	{
+		traveler = traveler->next;
+		total_node++;
+	}
+	return (total_node);
+}
+
