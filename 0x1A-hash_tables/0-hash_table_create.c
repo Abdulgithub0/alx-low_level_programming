a453ff07acc508bcafe716c171a9dec44d670391
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-06 21:39:37 +0100

    implemented a function that create a hashtable structure

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hashtable;
 
-	/*hash_node_t *hash_nodes;*/
+	unsigned long int i = 0;
 
 	new_hashtable = malloc(sizeof(hash_table_t));
-	/*hash_nodes = malloc(sizeof(hash_node_t) * size);*/
-	if (new_hashtable == NULL)
+	if (!new_hashtable)
 		return (NULL);
+	new_hashtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hashtable->array == NULL)
+	{
+		free(new_hashtable);
+		return (NULL);
+	}
 	new_hashtable->size = size;
-	new_hashtable->array = NULL;
+	while (i < size)
+	{
+		new_hashtable->array[i] = NULL;
+		i++;
+	}
 	return (new_hashtable);
 }
