64377660cdfc01474605553e5c265318ab06ef81
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-11 23:48:38 +0100

    used linear search to locate targer value

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - a function that searches for a value in an array of
+ * integers using the Linear search algorithm
+ * @array: is a pointer to the first element of the array to search
+ * @size: size of the array
+ * @value: target value to look for in the array
+ * Return: value if found or -1 if otherwise
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	if (array && size >= 1)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (array[i]);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*.......................standard libs used.........................*/
+#include <stdio.h>
+
+/*...................... function prototypes*.......................*/
+int linear_search(int *array, size_t size, int value);
+#endif 
