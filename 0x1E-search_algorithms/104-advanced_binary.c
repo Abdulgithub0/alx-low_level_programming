52294ad7db77a97196e030227ef574952b3b3e00
Bello Abdulsamad <117740814+Abdulgithub0@users.noreply.github.com>
2023-09-14 00:23:05 +0100

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,1 +1,48 @@
-#
+#include "search_algos.h"
+
+/**
+ * advance_search - a function that searches for a value in an array of
+ * integers using the binary search algorithm
+ * @array: is a pointer to the first element of the array to search
+ * @size: size of the array
+ * @value: target value to look for in the array
+ * Return: value if found or -1 if otherwise
+ */
+
+int advance_binary(int *array, size_t size, int value)
+{
+	int r_end, l_end, mid, i;
+
+  l_end = 0;
+  mid = 0;
+  r_end = size - 1;
+  while (l_end <= r_end)
+    {
+      printf("Searching in array: ");
+      for (i = l_end; i <= r_end; i++)
+        {
+          if (i != r_end)
+	  {
+            printf("%d, ", array[i]);
+	  }
+          else
+	  {
+            printf("%d\n", array[i]);
+	  }
+        } 
+      mid = (l_end + r_end) / 2;
+      if (array[mid] == value)
+      {
+        return (mid);
+      }
+      else if (value < array[mid])
+      {
+        r_end = mid - 1;
+      }
+      else
+      {
+        l_end = mid + 1;
+      }
+    }
+  return (-1);
+}
