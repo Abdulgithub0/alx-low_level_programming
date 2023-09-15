56c93ef38d1a79aa7fbbcf5a327896a04f1b8d10
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-15 17:05:27 +0100

    task 104

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,36 +13,36 @@ int binary_search(int *array, size_t size, int value)
 {
 	int r_end, l_end, mid, i;
 
-  l_end = 0;
-  mid = 0;
-  r_end = size - 1;
-  while (l_end <= r_end)
-    {
-      printf("Searching in array: ");
-      for (i = l_end; i <= r_end; i++)
-        {
-          if (i != r_end)
-	  {
-            printf("%d, ", array[i]);
-	  }
-          else
-	  {
-            printf("%d\n", array[i]);
-	  }
-        } 
-      mid = (l_end + r_end) / 2;
-      if (array[mid] == value)
-      {
-        return (mid);
-      }
-      else if (value < array[mid])
-      {
-        r_end = mid - 1;
-      }
-      else
-      {
-        l_end = mid + 1;
-      }
-    }
-  return (-1);
+	l_end = 0;
+	mid = 0;
+	r_end = size - 1;
+	while (l_end <= r_end)
+	{
+		printf("Searching in array: ");
+		for (i = l_end; i <= r_end; i++)
+		{
+			if (i != r_end)
+			{
+				printf("%d, ", array[i]);
+			}
+			else
+			{
+				printf("%d\n", array[i]);
+			}
+		}
+		mid = (l_end + r_end) / 2;
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (value < array[mid])
+		{
+			r_end = mid - 1;
+		}
+		else
+		{
+			l_end = mid + 1;
+		}
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,48 +1,57 @@
 #include "search_algos.h"
 
 /**
- * advance_search - a function that searches for a value in an array of
- * integers using the binary search algorithm
+* recursive_search - a function that searches for the first occurence of 
+* value in an array of integers using the binary search algorithm
+* @arr: is a pointer to the first element of the array to search
+* @start: first index of the array
+* @end: last index
+* @value: target value to look for in the array
+* Return: value index if found or -1 if otherwise
+*/
+
+int recursive_search(int arr[], int start, int end, int x)
+{
+	int mid, i;
+
+	mid = start + (end - start) / 2;
+	if (start > end)
+		return (-1);
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		if (i != end)
+			printf("%d, ", arr[i]);
+		else
+			printf("%d\n", arr[i]);
+	}
+	if (x == arr[mid])
+	{
+		if (mid == start || arr[mid - 1] != x)
+			return mid;
+		else
+			return recursive_search(arr, start, mid - 1, x);
+	}
+	else if (x < arr[mid])
+	{
+		return recursive_search(arr, start, mid - 1, x);
+	}
+	else
+	{
+		return recursive_search(arr, mid + 1, end, x);
+	}
+}
+
+/**
+ * advanced_search - a function that searches for the first occurence of 
+ * value in an array of integers using the binary search algorithm
  * @array: is a pointer to the first element of the array to search
  * @size: size of the array
  * @value: target value to look for in the array
  * Return: value if found or -1 if otherwise
  */
 
-int advance_binary(int *array, size_t size, int value)
+int advanced_binary(int *array, size_t size, int value)
 {
-	int r_end, l_end, mid, i;
-
-  l_end = 0;
-  mid = 0;
-  r_end = size - 1;
-  while (l_end <= r_end)
-    {
-      printf("Searching in array: ");
-      for (i = l_end; i <= r_end; i++)
-        {
-          if (i != r_end)
-	  {
-            printf("%d, ", array[i]);
-	  }
-          else
-	  {
-            printf("%d\n", array[i]);
-	  }
-        } 
-      mid = (l_end + r_end) / 2;
-      if (array[mid] == value)
-      {
-        return (mid);
-      }
-      else if (value < array[mid])
-      {
-        r_end = mid - 1;
-      }
-      else
-      {
-        l_end = mid + 1;
-      }
-    }
-  return (-1);
+	return recursive_search(array, 0, size - 1, value);
 }
