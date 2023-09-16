60fdb115cd2aa3ae299b2fbac0491e1d9e68d4ee
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-16 09:10:27 +0100

    fix betty

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,12 +1,12 @@
 #include "search_algos.h"
 
 /**
-* recursive_search - a function that searches for the first occurence of 
+* recursive_search - a function that searches for the first occurence of
 * value in an array of integers using the binary search algorithm
 * @arr: is a pointer to the first element of the array to search
 * @start: first index of the array
 * @end: last index
-* @value: target value to look for in the array
+* @x: target value to look for in the array
 * Return: value index if found or -1 if otherwise
 */
 
@@ -28,22 +28,22 @@ int recursive_search(int arr[], int start, int end, int x)
 	if (x == arr[mid])
 	{
 		if (mid == start || arr[mid - 1] != x)
-			return mid;
+			return (mid);
 		else
-			return recursive_search(arr, start, mid - 1, x);
+			return (recursive_search(arr, start, mid - 1, x));
 	}
 	else if (x < arr[mid])
 	{
-		return recursive_search(arr, start, mid - 1, x);
+		return (recursive_search(arr, start, mid - 1, x));
 	}
 	else
 	{
-		return recursive_search(arr, mid + 1, end, x);
+		return (recursive_search(arr, mid + 1, end, x));
 	}
 }
 
 /**
- * advanced_search - a function that searches for the first occurence of 
+ * advanced_binary - a function that searches for the first occurence of
  * value in an array of integers using the binary search algorithm
  * @array: is a pointer to the first element of the array to search
  * @size: size of the array
@@ -54,6 +54,6 @@ int recursive_search(int arr[], int start, int end, int x)
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array && size != 0 && value)
-		return recursive_search(array, 0, size - 1, value);
+		return (recursive_search(array, 0, size - 1, value));
 	return (-1);
 }
